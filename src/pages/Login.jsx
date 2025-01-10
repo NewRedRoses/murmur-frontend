d0133f6a98894d772d304a8acb797a4781bace60
Mark A. <mark001616@gmail.com>
2025-01-09 23:24:03 -0500

    Stylize login page to match mockup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,23 +2,28 @@ import Logo from "../../public/Logo.svg";
 import styles from "../styles/login.module.css";
 export default function Login() {
   return (
-    <div className="content">
+    <div className={styles["login-container"]}>
       <img src={Logo} alt="logo" className={styles.logo} />
       <h1 className={styles.text}>Login</h1>
-      <form action="/test" method="post">
-        <label htmlFor="username" className={styles.text}>
-          Username
-        </label>
-        <input type="text" id="username" name="username" />
+      <form action="/test" method="post" className={styles["login-form"]}>
+        <div className={styles.input}>
+          <label htmlFor="username" className={styles.text}>
+            Username
+          </label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className={styles.input}>
+          <label htmlFor="password" className={styles.text}>
+            Password
+          </label>
+          <input type="password" id="password" name="password" />
+        </div>
 
-        <label htmlFor="password" className={styles.text}>
-          Password
-        </label>
-        <input type="password" id="password" name="password" />
-
-        <button type="submit">Log In</button>
+        <button type="submit" className={styles["submit-btn"]}>
+          Log In
+        </button>
       </form>
-      <div className="signup-redirect">
+      <div className={styles["signup-link"]}>
         <span className={styles.text}>New?</span>
         <a href="/signup">Sign Up!</a>
       </div>
