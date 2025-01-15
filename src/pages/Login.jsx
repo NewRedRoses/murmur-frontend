a53152fc151f9310c5b5bd5f07022b652921779e
Mark A. <mark001616@gmail.com>
2025-01-14 23:55:39 -0500

    Make POST request to backend
    Redirect to / if succesful

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,83 @@
+import { useState } from "react";
 import Logo from "../../public/Logo.svg";
 import styles from "../styles/login.module.css";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const apiUrl = import.meta.env.VITE_APP_API_URL;
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    try {
+      e.preventDefault();
+      const response = await axios.post(
+        `${apiUrl}api/auth/login`,
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        },
+      );
+      console.log(response);
+
+      if (response.status === 200) {
+        console.log("Login success");
+        localStorage.setItem("token", response.data.token);
+        navigate("/");
+      } else {
+        alert("Login Failure");
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Unable to login");
+    }
+  }
+
   return (
     <div className={styles["login-container"]}>
       <img src={Logo} alt="logo" className={styles.logo} />
       <h1 className={styles.text}>Login</h1>
-      <form
-        action={apiUrl + "api/auth/login"}
-        method="post"
-        className={styles["login-form"]}
-      >
+
+      <form className={styles["login-form"]}>
         <div className={styles.input}>
           <label htmlFor="username" className={styles.text}>
             Username
           </label>
-          <input type="text" id="username" name="username" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className={styles.input}>
           <label htmlFor="password" className={styles.text}>
             Password
           </label>
-          <input type="password" id="password" name="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-        <button type="submit" className={styles["submit-btn"]}>
+        <button
+          type="submit"
+          className={styles["submit-btn"]}
+          onClick={handleLogin}
+        >
           Log In
         </button>
       </form>
