552abb88f5c28a6d4de136132ba04862d8037c08
Mark A. <mark001616@gmail.com>
2025-01-18 18:38:15 -0500

    Fetch convos from backend and pass to sidebar component

--- src/components/ConversationsSidebar.jsx
+++ src/components/ConversationsSidebar.jsx
@@ -1,42 +1,9 @@
 import styles from "../styles/conversationSidebar.module.css";
 import { Search, UserRound } from "lucide-react";
-export default function ConversationSidebar() {
-  const sampleData = [
-    {
-      id: 1,
-      senderName: "Mateo",
-      message: "Lorem ipsum",
-    },
-    {
-      id: 2,
-      senderName: "josephine",
-      message: "Lorem ipsum",
-    },
-    {
-      id: 3,
-      senderName: "luke",
-      message: "Lorem ipsum",
-    },
-    {
-      id: 4,
-      senderName: "john",
-      message: "Lorem ipsum",
-    },
-    {
-      id: 5,
-      senderName: "zeke",
-      message: "Lorem ipsum",
-    },
-    {
-      id: 6,
-      senderName: "abel",
-      message: "Lorem ipsum",
-    },
-  ];
-
+export default function ConversationSidebar({ conversations }) {
   return (
     <div className={styles["conversations-sidebar"]}>
-      <h1 className={styles.header}>Conversations</h1>
+      <h1 className={styles.header}>Conversations ({conversations.length})</h1>
 
       <div className={styles["searchbar-container"]}>
         <Search className={styles["search-icon"]} size="25" color="#34495e" />
@@ -46,20 +13,19 @@ export default function ConversationSidebar() {
           className={styles["searchbar-inputbox"]}
         />
       </div>
-
       <ol className={styles.conversations}>
-        {sampleData.map((message) => {
+        {conversations.map((conversation) => {
           return (
-            <li className={styles["conversation-card"]} key={message.id}>
+            <li className={styles["conversation-card"]} key={conversation.id}>
               <div className={styles["msg-card-left"]}>
                 <UserRound className={styles["msg-card-pfp"]} size="35" />
               </div>
               <div className={styles["msg-card-right"]}>
                 <div className={styles["msg-card-sender-name"]}>
-                  {message.senderName}
+                  {conversation.name} (@{conversation.username})
                 </div>
                 <div className={styles["msg-card-preview-text"]}>
-                  {message.message}
+                  {conversation.test}
                 </div>
               </div>
             </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,38 @@
 import ConversationSidebar from "../components/ConversationsSidebar";
 import Navbar from "../components/Navbar";
 import Chat from "../components/Chat";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, redirect } from "react-router-dom";
 export default function Home() {
+  const apiUrl = import.meta.env.VITE_APP_API_URL;
+  const [messages, setMessages] = useState([]);
+
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}api/messages`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+
+      .then((response) => {
+        if (response.status == 200) {
+          setMessages(response.data);
+        } else {
+          localStorage.clear();
+        }
+      });
+  }, []);
+
   return (
     <div className="content">
       <Navbar />
       <div className="container">
         <div className="left">
-          <ConversationSidebar />
+          <ConversationSidebar conversations={messages} />
         </div>
         <div className="right">
           <Chat />
