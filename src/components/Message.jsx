71f7487165cbe61605122230d0a25a9c6c18dbe6
Mark A. <mark001616@gmail.com>
2025-01-30 21:22:20 -0500

    Remove console log for type

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,6 @@ import styles from "../styles/chat.module.css";
 export default function Message({ id, content, type }) {
   return (
     <>
-      {console.log(type)}
       {type == "received" ? (
         <div className={styles["msg-incoming"]} key={id}>
           <div className={styles.msg}>{content}</div>
