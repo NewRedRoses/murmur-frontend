396caab71756ad2eb07473ba932c5c30a9b7d0c3
Mark A. <mark001616@gmail.com>
2025-01-30 21:22:52 -0500

    Add profile and logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
+import axios from "axios";
 import Logo from "../../public/Logo.svg";
 import styles from "../styles/navbar.module.css";
-import { User } from "lucide-react";
+import { User, LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+  const token = localStorage.getItem("token");
+  const apiUrl = import.meta.env.VITE_APP_API_URL;
+  const url = `${apiUrl}api/auth/logout`;
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    navigate(0);
+  }
   return (
     <div className={styles.navbar}>
       <a href="/">
         <img src={Logo} alt="logo" className={styles.logo} />
       </a>
-      <a href="/profile">
-        <User className={styles["profile-icon"]} />
-      </a>
+      <div className={styles["right"]}>
+        <a href="/profile">
+          <User className={styles["profile-icon"]} />
+        </a>
+        <button onClick={handleLogout} className={styles["logout-btn"]}>
+          <LogOut className={styles["logout-icon"]} />
+        </button>
+      </div>
     </div>
   );
 }
